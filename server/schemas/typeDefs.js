da44611262a7fe241d0995c7cd62809ea65d77a4
Marcus Ikegami <hikegami.6@gmail.com>
2022-02-03 10:27:12 -0700

    Backend functionality in place

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,6 @@
 const { gql } = require('apollo-server-express');
 
+
 const typeDefs = gql`
     type Thought {
         _id: ID
@@ -26,11 +27,25 @@ const typeDefs = gql`
         friends: [User]
     }
 
+    type Auth {
+        token: ID!,
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
