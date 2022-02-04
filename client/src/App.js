4321cdd90d9430b67f4b7484fa8ffb9757b77a5a
Marcus Ikegami <hikegami.6@gmail.com>
2022-02-04 10:02:36 -0700

    added react router, friendlist component, and new queries

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 // ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
 // InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
 // createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -20,15 +26,23 @@ const client = new ApolloClient({
 
 function App() {
   return (
-  <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+           <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile/:username?" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+
+            <Route component={NoMatch}/>
+           </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql`
+const QUERY_THOUGHTS = gql`
 query thoughts($username: String) {
     thoughts(username: $username) {
       _id
@@ -13,6 +13,48 @@ query thoughts($username: String) {
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+
+`;
+
+const QUERY_THOUGHT = gql`
+query thought($id: ID!) {
+    thought(_id: $id) {
+        _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+}
+`;
+
+export {QUERY_THOUGHT, QUERY_THOUGHTS};
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
